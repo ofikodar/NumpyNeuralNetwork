10c248b8e1970cc02f492f0c78194316896592bc
ofikodar <ofikodar@gmail.com>
2021-06-27 12:18:55 +0300

    Add pool to layers.py

--- layers/layers.py
+++ layers/layers.py
@@ -1,7 +1,7 @@
 from layers.activations import Sigmoid, LeakyRelu, Softmax
 from layers.conv2d import Conv2DLayer
 from layers.fully_connected import FCLayer
-from layers.reshape import FlattenLayer, MaxPollLayer
+from layers.reshape import FlattenLayer, MaxPoolLayer
 
 layers_type_dict = dict()
 layers_type_dict['fc'] = FCLayer
