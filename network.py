e841438df11d850b9cbfcfaad6ad2f546a3fec8f
ofikodar <ofikodar@gmail.com>
2021-06-27 19:12:25 +0300

    Add agreegated acc and loss train

--- network.py
+++ network.py
@@ -67,9 +67,14 @@ class Model:
 
         for e in range(epochs):
             print("Epoch num:", e)
+
+            epoch_correct_samples = 0
+            epoch_aggregated_log = 0
+
             np.random.shuffle(idx)
             _X_train = X_train[idx]
             _y_train = y_train[idx]
+
             for batch_idx in range(num_batches):
                 batch_x = _X_train[batch_idx * batch_size:(batch_idx + 1) * batch_size]
                 batch_y = _y_train[batch_idx * batch_size:(batch_idx + 1) * batch_size]
@@ -77,11 +82,15 @@ class Model:
                     y = np.expand_dims(y, axis=1)
                     if len(x.shape) == 1:
                         x = np.expand_dims(x, axis=1)
-                    self.predict(x)
+                    p = self.predict(x)
                     self.backprop(x, y)
+                    epoch_aggregated_log, epoch_correct_samples = self.update_epoch_training_history(p, y,
+                                                                                                     epoch_aggregated_log,
+                                                                                                     epoch_correct_samples)
+
                 self.update_weights(lr)
-            train_loss, train_acc = self.report(X_train, y_train, "Train")
-            val_loss, val_acc = self.report(X_test, y_test, "Val")
+            train_loss, train_acc = self.train_report(X_train, epoch_aggregated_log, epoch_correct_samples)
+            val_loss, val_acc = self.val_report(X_test, y_test)
 
             history['train_acc'].append(train_acc)
             history['train_loss'].append(train_loss)
@@ -127,7 +136,25 @@ class Model:
             plt.savefig(output_dir + f"{key} - {self.model_name}.jpg")
             plt.clf()
 
-    def report(self, x_data, y_data, name):
+    def update_epoch_training_history(self, p, y, loss, acc):
+        loss += self.cross_entropy(p, np.expand_dims(y, 1))
+        if np.argmax(p) == np.argmax(y):
+            acc += 1
+        return loss, acc
+
+    def train_report(self, x_data, loss, acc):
+        name = 'Train'
+        loss /= x_data.shape[0]
+        acc /= x_data.shape[0]
+        acc = 100 * acc
+
+        print(f"{name} loss:", round(loss, 4))
+        print(f"{name} Acc:", round(acc, 4))
+
+        return loss, acc
+
+    def val_report(self, x_data, y_data):
+        name = "Val"
         loss = 0
         acc = 0
         for x, y in zip(x_data, y_data):
