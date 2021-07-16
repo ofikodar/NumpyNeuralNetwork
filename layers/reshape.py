4d6ad589f1321d2af2fdaf17e02fdee9745cefd8
ofikodar <ofikodar@gmail.com>
2021-07-16 13:41:51 +0300

    fixed maxpooling

--- layers/reshape.py
+++ layers/reshape.py
@@ -27,74 +27,69 @@ class MaxPoolLayer:
         self.cache, pool_output = _compiled_forward(layer_input, self.pool_size)
         return pool_output
 
-    def derive(self, dz):
-        dz = _compiled_derive(self.cache, dz, self.pool_size)
-        return dz
+    def derive(self, dx):
+        dx = _compiled_derive(self.cache, dx, self.pool_size)
+        return dx
 
     def update_weights(self, lr):
         pass
 
 
-@jit(nopython=True)
+# @jit(nopython=True)
 def _compiled_forward(layer_input, pool_size):
-    C, H, W = layer_input.shape
-    PH = pool_size
-    PW = pool_size
-    stride = pool_size
-    outH = int(1 + (H - PH) / stride)
-    outW = int(1 + (W - PW) / stride)
+    batch_size, channels, image_h, image_w = layer_input.shape
+    pool_out_h = int(1 + (image_h - pool_size) / pool_size)
+    pool_out_w = int(1 + (image_w - pool_size) / pool_size)
 
-    pool_output = np.zeros((C, outH * outW))
-    neuron = 0
-    for i in range(0, H - PH + 1, stride):
-        for j in range(0, W - PW + 1, stride):
-            pool_region = layer_input[:, i:i + PH, j:j + PW].copy().reshape(C, PH * PW)
-            pool_output[:, neuron] = pool_region.max()
-            neuron += 1
-    pool_output = pool_output.reshape(C, outH, outW)
+    # create output tensor for pooling layer
+    pool_output = np.zeros((batch_size, channels, pool_out_h, pool_out_w))
+    for index in range(batch_size):
+        out_col = np.zeros((channels, pool_out_h * pool_out_w))
+        neuron = 0
+        for i in range(0, image_h - pool_size + 1, pool_size):
+            for j in range(0, image_w - pool_size + 1, pool_size):
+                pool_region = layer_input[index, :, i:i + pool_size, j:j + pool_size].copy().reshape(channels, -1)
+                out_col[:, neuron] = pool_region.max(axis=1)
+                neuron += 1
+        pool_output[index] = out_col.copy().reshape(channels, pool_out_h, pool_out_w)
 
     cache = layer_input
     return cache, pool_output
 
 
-@jit(nopython=True)
-def _compiled_derive(cache, dout, pool_size):
+# @jit(nopython=True)
+def _compiled_derive(cache, dx, pool_size):
+    layer_input = cache
+    batch_size, channels, pool_output_h, pool_output_w = dx.shape
+    image_h, image_w = layer_input.shape[2], layer_input.shape[3]
 
-    x = cache
-    C, outH, outW = dout.shape
-    H, W = x.shape[1], x.shape[2]
-    PH = pool_size
-    PW = pool_size
-    stride = pool_size
-    # initialize gradient
-    dx = np.zeros(x.shape)
+    dx_next = np.zeros(layer_input.shape)
 
-    dout_row = dout.copy().reshape(C, outH * outW)
-    neuron = 0
-    for i in range(0, H - PH + 1, stride):
-        for j in range(0, W - PW + 1,stride):
-            pool_region = x[:, i:i + PH, j:j + PW].copy().reshape(C, PH * PW)
-            dmax_pool = np.zeros(pool_region.shape)
-
-            for idx in range(C):
-                max_pool_idx = int(pool_region[idx,:].argmax())
+    for index in range(batch_size):
+        dout_row = dx[index].reshape(channels, -1)
+        neuron = 0
+        for i in range(0, image_h - pool_size + 1, pool_size):
+            for j in range(0, image_w - pool_size + 1, pool_size):
+                pool_region = layer_input[index, :, i:i + pool_size, j:j + pool_size].reshape(channels, -1)
+                max_pool_indices = pool_region.argmax(axis=1)
                 dout_cur = dout_row[:, neuron]
-                dmax_pool[idx, max_pool_idx] = dout_cur[0]
-            neuron +=1
-            dx[:, i:i + PH, j:j + PW] += dmax_pool.copy().reshape(C, PH, PW)
+                neuron += 1
+
+                dmax_pool = np.zeros(pool_region.shape)
+                dmax_pool[np.arange(channels), max_pool_indices] = dout_cur
+                dx_next[index, :, i:i + pool_size, j:j + pool_size] += dmax_pool.reshape(channels, pool_size, pool_size)
+    dx = dx_next
     return dx
 
 
 if __name__ == '__main__':
     np.random.seed(42)
     desc = {"type": "maxPooling",
-            "pool_size": 8}
+            "pool_size": 2}
     l = MaxPoolLayer(desc)
-    inp = (np.random.randn(1, 16, 16) * 10).astype(int)
+    inp = (np.random.randn(1, 1, 8, 8) * 10).astype(int)
     import time
-    print(inp)
     x = l.forward(inp)
     s_t = time.time()
-    print(x)
     zzz = l.derive(x)
-    print(time.time() - s_t)
+    # print(time.time() - s_t)
