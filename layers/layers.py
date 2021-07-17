b4a0fe060af4f29d2f1ede62a0c917077efee2dd
ofikodar <ofikodar@gmail.com>
2021-07-17 22:33:21 +0300

    Add bn to layers.py

--- layers/layers.py
+++ layers/layers.py
@@ -1,4 +1,5 @@
 from layers.activations import Sigmoid, LeakyRelu, Softmax
+from layers.batch_norm import BNLayer
 from layers.conv2d import Conv2DLayer
 from layers.fully_connected import FCLayer
 from layers.reshape import FlattenLayer, MaxPoolLayer
@@ -6,6 +7,7 @@ from layers.reshape import FlattenLayer, MaxPoolLayer
 layers_type_dict = dict()
 layers_type_dict['fc'] = FCLayer
 layers_type_dict['conv2d'] = Conv2DLayer
+layers_type_dict['bn'] = BNLayer
 layers_type_dict['flatten'] = FlattenLayer
 layers_type_dict['sigmoid'] = Sigmoid
 layers_type_dict['leakyRelu'] = LeakyRelu
